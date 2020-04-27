9d2a5f14ce1fcd6856ad9bfc3259904a98ed78a9
xuzhao <xuzhao@zhique.design>
2020-04-27 22:08:35 +0800

    重构

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,20 @@
-const { override, fixBabelImports, addLessLoader, addWebpackPlugin  } = require('customize-cra');
+const {
+    override,
+    fixBabelImports,
+    addLessLoader,
+    addWebpackAlias,
+    addDecoratorsLegacy,
+    useBabelRc,
+    useEslintRc,
+    addWebpackPlugin,
+    enableEslintTypescript,
+    addTslintLoader
+} = require('customize-cra');
+
+const path = require('path');
 const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const chalk = require('chalk');
 
 module.exports = override(
     fixBabelImports('import', {
@@ -7,9 +22,26 @@ module.exports = override(
         libraryDirectory: 'es',
         style: true,
     }),
+    // 支持webpack alias
+    addWebpackAlias({
+        '@': path.resolve(__dirname, 'src'),
+    }),
+    // 支持装饰器
+    addDecoratorsLegacy(),
+    useBabelRc(),
+    // useEslintRc(path.resolve(__dirname, '.eslintrc.js')),
+    enableEslintTypescript(),
+    // addTslintLoader(),
     addLessLoader({
         javascriptEnabled: true,
         modifyVars: { '@primary-color': '#1DA57A' },
     }),
-    addWebpackPlugin(new AntdDayjsWebpackPlugin())
+    addWebpackPlugin(new AntdDayjsWebpackPlugin()),
+    // 添加build进度条
+    addWebpackPlugin(new ProgressBarPlugin({
+            complete: "█",
+            format: `${chalk.green('Building')} [ ${chalk.green(':bar')} ] ':msg:' ${chalk.bold('(:percent)')}`,
+            clear: true
+        })
+    )
 );
--- src/ZhiQue.tsx
+++ src/ZhiQue.tsx
@@ -1,13 +1,13 @@
 import React, { Suspense } from 'react';
-import { RouterProps } from 'react-router';
 import { Router } from 'dva/router';
+import { RouterProps } from 'react-router';
 import routers from './config/routers';
 import AuthorizedRoute from './components/Authorized/AuthorizedRoute';
 
 const ZhiQue: React.FC<RouterProps> = ({ history }) => (
     <Router history={history}>
         <Suspense fallback={<div>loading...</div>}>
-            <AuthorizedRoute routers={routers}/>
+            <AuthorizedRoute routers={routers} />
         </Suspense>
     </Router>
 )
